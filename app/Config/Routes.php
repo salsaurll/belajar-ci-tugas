db2172cc65a070aeb4497ac3de777629f565f753
salsaurll <salsaaurellia1@gmail.com>
2025-06-15 18:59:49 +0700

    first commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,22 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+
+$routes->get('/', 'Home::index', ['filter' => 'auth']);
+
+$routes->get('login', 'AuthController::login');
+$routes->post('login', 'AuthController::login');
+$routes->get('logout', 'AuthController::logout');
+
+$routes->group('produk', ['filter' => 'auth'], function ($routes) { 
+    $routes->get('', 'ProdukController::index');
+    $routes->post('', 'ProdukController::create');
+    $routes->post('edit/(:any)', 'ProdukController::edit/$1');
+    $routes->get('delete/(:any)', 'ProdukController::delete/$1');
+});
+
+$routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
